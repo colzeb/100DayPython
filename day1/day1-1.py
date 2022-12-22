ad13fd896ac3b94f1840e1c79a4e96e5abc42265
pankaj <hi.p@outlook.com>
2022-12-22 19:13:12 +0530

    day 1 task 1

---
+++ day1/day1-1.py
@@ -0,0 +1,9 @@
+#Write your code below this line 👇
+# Basic printing
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
+
+print("Day 1 - Python Print Function\n"+
+"The function is declared like this:\n"+
+"print('what to print')")
