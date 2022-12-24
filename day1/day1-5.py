fbc15b5aa948aabd0e1bb76ebefdd622e634f8e5
pankaj <hi.p@outlook.com>
2022-12-24 13:50:26 +0530

    day 1 task 5

---
+++ day1/day1-5.py
@@ -0,0 +1,5 @@
+#Go to: https://replit.com/@appbrewery/band-name-generator-start?v=1
+city = input("Name your city ? \n")
+pet = input("Name your pet? \n")
+
+print("Your Band name can be " + city + " " + pet)
